0bc8bfb6df36732ea88dfb7558c9a51e037603a1
seleneabraldez <seleabraldez[1;7rgmail.com>
2018-09-07 15:25:27 -0300

    primer trabajo practico

---
+++ abraldezCalculadora.c
@@ -0,0 +1,56 @@
+/** \brief Suma dos numeros
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Resultado de suma num1+num2
+ *
+ */
+
+ float addition (float num1, float num2);
+
+
+
+ /** \brief Resta dos numeros
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Resultado de resta num1-num2
+ *
+ */
+
+ float subtraction (float num1, float num2);
+
+
+
+/** \brief Divide dos numeros
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Resultado de division num1/num2
+ *
+ */
+
+float division (float num1, float num2);
+
+
+
+  /** \brief Producto dos numeros
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Resultado de multiplicacion num1*num2
+ *
+ */
+
+ float multiplication (float num1, float num2);
+
+
+
+  /** \brief Factorial de un numero
+ *
+ * \param Numero 1
+ * \return Resultado de factorial numero 1
+ *
+ */
+
+int factorial (float num1);
